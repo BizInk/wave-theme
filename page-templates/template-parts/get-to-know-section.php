2fa32d8cdf8075491b413a1ad00b183cc98ba44b
Jayden Major <jayden@jaydenmajor.com>
2025-06-13 11:20:46 +1000

    Teammember Fix

--- page-templates/template-parts/get-to-know-section.php
+++ page-templates/template-parts/get-to-know-section.php
@@ -63,10 +63,10 @@ $show_company = get_sub_field('show_company') ? true : false;
                             <?php } ?>
                             <h4 class="member-name"><?php echo $team_member->post_title; ?></h4>
                             <?php if( !empty($member_position) && $show_position ){ ?>
-                                <h6 class="designation"><?= $member_position; ?></h6>
+                                <h6 class="designation"><?php echo $member_position; ?></h6>
                             <?php }
                             if( !empty($member_company) && $show_company ){ ?>
-                                <p><?= $member_company; ?></p>
+                                <p><?php echo $member_company; ?></p>
                             <?php }
                             if($enable_team_link){ ?>
                                 </a>
@@ -74,9 +74,9 @@ $show_company = get_sub_field('show_company') ? true : false;
                             $facebook = get_field('member_facebook', $team_member);
                             $twitter = get_field('member_twitter', $team_member);
                             $linkedin = get_field('member_linkedin', $team_member);
-                            $member_phone = get_field('member_phone', $our_member); 
-                            $member_email = get_field('member_email', $our_member); 
-                            if( $show_social && ($facebook || $twitter || $linkedin || $member_phonea || $member_email ) ){ ?>
+                            $member_phone = get_field('member_phone', $team_member); 
+                            $member_email = get_field('member_email', $team_member);
+                            if( $show_social && ( $facebook || $twitter || $linkedin || $member_phone || $member_email ) ){ ?>
                             <div class="social-nav">
                                 <?php 
                                 if( !empty($facebook) ){ 
@@ -89,10 +89,10 @@ $show_company = get_sub_field('show_company') ? true : false;
                                     ?><a href="<?php echo $linkedin; ?>" target="_blank"><i aria-label="LinkedIn" class="fa fa-linkedin-square" aria-hidden="true"></i></a><?php 
                                 }
                                 if( !empty($member_phone) ){ 
-                                    ?><a href="tel:<?= $member_phone; ?>" target="_blank" title="<?= $member_phone; ?>"><i class="fa fa-phone" aria-hidden="true"></i></a><?php
+                                    ?><a href="tel:<?php echo $member_phone; ?>" target="_blank" title="<?php echo $member_phone; ?>"><i aria-label="Phone" class="fa fa-phone" aria-hidden="true"></i></a><?php
                                 }
                                 if( !empty($member_email) ){
-                                    ?><a href="mailto:<?= $member_email; ?>" target="_blank" title="<?= $member_email; ?>"><i class="fa fa-envelope" aria-hidden="true"></i></a><?php
+                                    ?><a href="mailto:<?php echo $member_email; ?>" target="_blank" title="<?php echo $member_email; ?>"><i aria-label="Email" class="fa fa-envelope" aria-hidden="true"></i></a><?php
                                 }
                                 ?>
                             </div>
